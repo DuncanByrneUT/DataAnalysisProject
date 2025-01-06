d6e62d89f5772bc4e3f1171c80d3dbde6dc56f5b
DuncanByrneUT <duncanbyrne7254@gmail.com>
2025-01-06 13:00:06 -0500

    added code to remove the duplicates and convert the data types if necessary

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ import pandas as pd
 # pandas is reading the data in the csv file
 df = pd.read_csv('netflix_titles.csv')
 
-print(df.isnull().sum())  # Shows count of missing values per col
+print(df.isnull().sum())
+
+df = df.dropna()
+
+# removing duplicates in the data
+df = df.drop_duplicates()
+
+# converting the data types if necessary
+df['column_name'] = df['column_name'].astype('category') # this is forthe categorical data
