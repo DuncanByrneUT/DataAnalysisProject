5f160bbaf8b01388c4b03e4fbfb30ae857bb400d
DuncanByrneUT <duncanbyrne7254@gmail.com>
2025-01-06 14:25:10 -0500

    added functionality that makes the code print data in the terminal,

--- index.py
+++ index.py
@@ -4,12 +4,17 @@ import pandas as pd
 # pandas is reading the data in the csv file
 df = pd.read_csv('netflix_titles.csv')
 
-print(df.isnull().sum())
+print(df.isnull().sum())  # Shows count of missing values per column
 
+# this decides whether the missing values wil either be be dropped, filled, or replaced
 df = df.dropna()
 
 # removing duplicates in the data
 df = df.drop_duplicates()
 
 # converting the data types if necessary
-df['column_name'] = df['column_name'].astype('category') # this is forthe categorical data
+df['type'] = df['type'].astype('category') # this is forthe categorical data
+
+print(df.describe())
+
+
