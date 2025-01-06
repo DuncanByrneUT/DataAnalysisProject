3cf5dfe358c2b5d03234f07c914e3f414bbcbf98
DuncanByrneUT <duncanbyrne7254@gmail.com>
2025-01-06 12:47:34 -0500

    testing if this commits

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import pandas as pd
 # pandas is reading the data in the csv file
 df = pd.read_csv('netflix_titles.csv')
 
-print(df.isnull().sum())  # Shows count of missing values per column
+print(df.isnull().sum())  # Shows count of missing values per col
